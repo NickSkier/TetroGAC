41a8d870424cc833d5ea521abce0b508f4c47ae4
NickSkier <nickskier02@gmail.com>
2025-01-08 13:03:01 +0300

    Move: ncurses colors initialization to Game class

--- src/Game.cpp
+++ src/Game.cpp
@@ -335,3 +335,13 @@ void Game::gameControls(const int userInput) {
 	tetromino.update(&field);
 }
 
+void Game::initColors() {
+	init_color(COLOR_GREEN, 0, 500, 0);
+	init_color(COLOR_ORANGE, 1000, 300, 0);
+	init_pair(1, COLOR_CYAN, COLOR_CYAN);
+	init_pair(2, COLOR_BLUE, COLOR_BLUE);
+	init_pair(3, COLOR_ORANGE, COLOR_ORANGE);
+	init_pair(4, COLOR_YELLOW, COLOR_YELLOW);
+	init_pair(5, COLOR_GREEN, COLOR_GREEN);
+	init_pair(6, COLOR_RED, COLOR_RED);
+	init_pair(7, COLOR_MAGENTA, COLOR_MAGENTA);
--- src/main.cpp
+++ src/main.cpp
@@ -12,15 +12,15 @@ int main() {
 	nodelay(stdscr, TRUE);
 	start_color();
 
-	init_color(COLOR_GREEN, 0, 500, 0);
-	init_color(COLOR_ORANGE, 1000, 300, 0);
-	init_pair(1, COLOR_CYAN, COLOR_CYAN);
-	init_pair(2, COLOR_BLUE, COLOR_BLUE);
-	init_pair(3, COLOR_ORANGE, COLOR_ORANGE);
-	init_pair(4, COLOR_YELLOW, COLOR_YELLOW);
-	init_pair(5, COLOR_GREEN, COLOR_GREEN);
-	init_pair(6, COLOR_RED, COLOR_RED);
-	init_pair(7, COLOR_MAGENTA, COLOR_MAGENTA);
+	// init_color(COLOR_GREEN, 0, 500, 0);
+	// init_color(COLOR_ORANGE, 1000, 300, 0);
+	// init_pair(1, COLOR_CYAN, COLOR_CYAN);
+	// init_pair(2, COLOR_BLUE, COLOR_BLUE);
+	// init_pair(3, COLOR_ORANGE, COLOR_ORANGE);
+	// init_pair(4, COLOR_YELLOW, COLOR_YELLOW);
+	// init_pair(5, COLOR_GREEN, COLOR_GREEN);
+	// init_pair(6, COLOR_RED, COLOR_RED);
+	// init_pair(7, COLOR_MAGENTA, COLOR_MAGENTA);
 
 	Game game;
 
@@ -30,4 +30,3 @@ int main() {
 
 	return 0;
 }
-
